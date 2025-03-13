ab9845c92751f0584a32b02db1f57f18ca96b712
Michael <mlusz1601@gmail.com>
2025-03-13 15:38:08 +0100

    Add files via upload

---
+++ cmds/echo.h
@@ -0,0 +1,14 @@
+/*
+ * echo.h
+ * Print request
+ * echo.h is the part of TSH.
+ *
+ * Written by Michael Ł. (Micha1207) in GNU Emacs.
+ * 12.03.2025
+*/
+#include <stdio.h>
+#include <string.h>
+
+void echo(const char *text){
+  printf("%s\n", text);
+}
