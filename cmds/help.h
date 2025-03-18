a652f6a06f3e0f712a605b0e02c0cf0be72ba9b9
Michael <mlusz1601@gmail.com>
2025-03-18 21:24:44 +0100

    Update help.h

--- cmds/help.h
+++ cmds/help.h
@@ -16,6 +16,8 @@ void help(){
   printf("  cat <file> ---------- Read and display file content (conCATinate). \n");
   printf("  pwd  ---------------- Print Working Directory. \n");
   printf("  echo <what?> -------- Echo what is after the command (e.g. echo Hello => Hello. Supports only one word) \n");
+  printf("  whoami --------- print username.\n");
+  printf("  mkdir ---------- make directory. \n");
   printf("  help ----------- print this. \n");
   printf("  exit ----------- exit TSH. \n");
 }
